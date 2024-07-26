3717d670d46bf307ac695c2bc55a4a3aafae8259
Umar-Shahzad6967 <152487083+Umar-Shahzad6967@users.noreply.github.com>
2024-07-26 20:08:01 +0500

    first commit

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -37,7 +37,7 @@ function Main() {
                 <div className="row mt-4 text-center">
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
                         <div className="card" style={{ width: '18rem', }}>
-                            <img src="https://t3.ftcdn.net/jpg/00/07/32/48/360_F_7324855_mx4CEBWTr81XLOrlQccCROtP2uNR7xbk.jpg" className="card-img-top" alt="..." />
+                            <img src="1.jpeg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Admin</h5>
 
@@ -49,7 +49,7 @@ function Main() {
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
                         <div className="card" style={{ width: '18rem' }}>
-                            <img src="https://img.freepik.com/free-vector/teacher-standing-near-blackboard-holding-stick-isolated-flat-vector-illustration-cartoon-woman-character-near-chalkboard-pointing-alphabet_74855-8600.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1721952000&semt=sph" className="card-img-top" alt="..." />
+                            <img src="2.jpeg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Teacher</h5>
                                 <p className="card-text">If you are looking All details moving on Dashbaord </p>
@@ -60,7 +60,7 @@ function Main() {
                     </div>
                     <div className="col-lg-4 col-md-6 col-sm-12 mb-3">
                         <div className="card" style={{ width: '18rem' }}>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8fcBxxWM3b_a2Ig9vqW8C6eW7U34fXM9hUw9FpHhzQX3ZMpPFL6ZBvizSOodN0OpbIaU&usqp=CAU" className="card-img-top" alt="..." />
+                            <img src="3.jpeg" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">Student</h5>
                                 <p className="card-text">If you are looking All details moving on Dashbaord </p>
